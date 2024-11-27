13d44ba9ce2cabcebcaf5efc5440a8cc6a7670ab
昂热 <hilbertanger@gmail.com>
2024-11-27 09:49:04 +0800

    fix: build

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -20,7 +20,7 @@ module.exports = merge(webpackBaseConfig, {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': 'production'
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
   ]
 });

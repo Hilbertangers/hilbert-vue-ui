6cc0acff92d6aaa9279c1af758fbcc2ed119bb31
昂热 <hilbertanger@gmail.com>
2025-04-10 11:29:27 +0800

    feat: PinInput examples

--- examples/view/pin-input.vue
+++ examples/view/pin-input.vue
@@ -1,17 +1,42 @@
 <template>
-  <pin-input
-    v-model="pin"
-    type="number"
-    :length="6"
-    @complete="handleComplete"
-  />
+  <div>
+    <div style="margin-bottom: 10px;">
+      数字类型：
+      <pin-input
+        v-model="pin1"
+        type="number"
+        :length="6"
+        @complete="handleComplete"
+      />
+    </div>
+    <div style="margin-bottom: 10px;">
+      数字字母类型：
+      <pin-input
+        v-model="pin2"
+        type="text"
+        :length="4"
+        @complete="handleComplete"
+      />
+    </div>
+    <div style="margin-bottom: 10px;">
+      密码类型：
+      <pin-input
+        v-model="pin3"
+        type="password"
+        :length="6"
+        @complete="handleComplete"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      pin: ''
+      pin1: '',
+      pin2: '',
+      pin3: '',
     }
   },
   methods: {
